5830dbd04f771b93e077106c31137c0e9242b4fb
IsaacMgr <gauravthapa926@gmail.com>
2023-01-08 14:45:44 +0545

    commit

---
+++ Constructors/const in derived class/prac.cpp
@@ -0,0 +1,55 @@
+//constructor in derived class in single inheritance
+#include <iostream>
+using namespace std;
+class Parent
+{
+    protected:
+        int x;
+    public:
+        Parent()
+        {
+            x=0;
+            cout<<"\nInside default constructor of parent"<<endl;
+        }
+        Parent(int x)
+        {
+            this->x=x;
+            cout<<"\nInside parametarized constructor of parent"<<endl;
+        }
+        void displayParent()
+        {
+            cout<<"\nValue of x="<<x<<endl;
+        }
+        
+};
+class Child:public Parent
+{
+    int y;
+    public:
+        Child()
+        {
+            y=0;
+            cout<<"Inside default constructor of child"<<endl;
+        }
+
+Child(int x,int y):Parent(x)
+{
+    this->y=y;
+    cout<<"\nInside parametarized constructor of child"<<endl;
+}
+void displaychild()
+{
+    displayParent();
+    cout<<"\nThe value of y="<<y<<endl;
+}
+};
+int main()
+{
+    Parent pobj(10);
+    pobj.displayParent();
+    Child cobj(55,65);
+    cobj.displaychild();
+    Child cobj2;
+    cobj2.displaychild();
+    return 0;
+}
---
+++ Constructors/const in derived class/prac1.cpp
@@ -0,0 +1,77 @@
+//Constructor in derived class in Multilevel inheritance
+#include <iostream>
+using namespace std;
+class Parent
+{
+    protected:
+        int x;
+    public:
+        Parent()
+        {
+            cout<<"\nInside default constructor of parent"<<endl;
+
+        }
+        Parent(int x)
+        {
+            this->x=x;
+            cout<<"\nInside parametarized constructor of parent"<<endl;
+        }
+        void displayparent()
+        {
+            cout<<"\nValue of x="<<x<<endl;
+        }
+
+};
+
+class Child:public Parent
+{
+    protected:
+        int y;
+    public:
+        Child()
+        {
+            y=0;
+            cout<<"\nInside default constructor of child"<<endl;
+        }
+        
+        Child(int x,int y):Parent(x)//in this code first Parent(x) will run then only statements inside Child(int x,int y)...
+        {
+            this->y=y;
+            cout<<"\nInside parametarized constructor of child"<<endl;
+        }
+        void displaychild()
+        {
+            displayparent();
+            cout<<"\nValue of y="<<y<<endl;
+        }
+};
+
+class Grandchild:public Child
+{
+    int z;
+    public:
+        Grandchild()
+        {
+            z=0;
+            cout<<"\nInside default constructor of grandchild"<<endl;
+        }
+        Grandchild(int x,int y,int z):Child(x,y)//in this code first Parent(x) will run then only statements inside Grandchild(int x,int y)...
+        {
+            this->z=z;
+            cout<<"\nInside default constructor of grandchild"<<endl;
+        }
+        void displaygrandchild()
+        {
+            displayparent();
+            displaychild();
+            cout<<"\nValue of z="<<z<<endl;
+        }
+};
+
+int main()
+{
+    Grandchild gobj0;
+    Grandchild gobj(55,95,105);
+    gobj.displaygrandchild();
+    return 0;
+}
